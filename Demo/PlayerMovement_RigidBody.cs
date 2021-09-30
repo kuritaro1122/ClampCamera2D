0426fe5fab1e4bbb3811978df94af86e5b2be81d
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2021-09-30 17:51:03 +0900

    Create PlayerMovement_RigidBody.cs

---
+++ Demo/PlayerMovement_RigidBody.cs
@@ -0,0 +1,25 @@
+public class PlayerMovement_RigidBody : MonoBehaviour {
+    [Header("--- GameObject ---")]
+    [SerializeField] ClampCamera2D cc2d;
+    [SerializeField] Rigidbody rb;
+    [Header("--- Movement ---")]
+    [SerializeField] float speed = 10f;
+
+    void Start() {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    void Update() {
+        Movement();
+    }
+
+    private void Movement() {
+        Vector2 stick;
+        stick.x = Input.GetAxis("Horizontal");
+        stick.y = Input.GetAxis("Vertical");
+        Vector3 pos = rb.position = cc2d.ClampPosition(rb.position);
+        Vector3 velocity = speed * (cc2d.LocalHorizontal(pos) * stick.x + cc2d.LocalVertical(pos) * stick.y);
+        float deltaTime = Time.deltaTime;
+        Vector3 pos_ = cc2d.ClampPosition(pos + velocity * deltaTime);
+        rb.velocity = (pos_ - pos) / deltaTime;
+    }
+}
